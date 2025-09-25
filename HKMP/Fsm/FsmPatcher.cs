72da9f4ac92d152cb3f89d6a44cf02be39a126c6
Grant Poquiz <gpoquiz@gmail.com>
2025-09-24 19:32:27 -0500

    FsmPatcher Update

--- HKMP/Fsm/FsmPatcher.cs
+++ HKMP/Fsm/FsmPatcher.cs
@@ -1,3 +1,4 @@
+using HarmonyLib;
 using Hkmp.Util;
 using HutongGames.PlayMaker.Actions;
 using Logger = Hkmp.Logging.Logger;
@@ -9,24 +10,17 @@ namespace Hkmp.Fsm;
 /// </summary>
 internal class FsmPatcher {
     /// <summary>
-    /// Registers the hooks necessary to patch.
-    /// </summary>
-    public void RegisterHooks() {
-        On.PlayMakerFSM.OnEnable += OnFsmEnable;
-    }
-
-    /// <summary>
     /// Callback method for the PlayMakerFSM#OnEnable hook.
     /// </summary>
     /// <param name="orig">The original method.</param>
-    /// <param name="self">The PlayMakerFSM instance that the hooked method was called on.</param>
-    private void OnFsmEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
-        orig(self);
-
+    /// <param name="__instance">The PlayMakerFSM instance that the hooked method was called on.</param>
+    [HarmonyPatch(typeof(PlayMakerFSM), nameof(PlayMakerFSM.OnEnable))]
+    [HarmonyPostfix]
+    private void PostfixOnEnable(PlayMakerFSM __instance) {
         // Check if it is a FSM for picking up shiny items
-        if (self.name.Equals("Inspect Region") && self.Fsm.Name.Equals("inspect")) {
+        if (__instance.name.Equals("Inspect Region") && __instance.Fsm.Name.Equals("inspect")) {
             // Find the action that checks whether the player enters the pickup area
-            var triggerAction = self.GetFirstAction<Trigger2dEvent>("Out Of Range");
+            var triggerAction = __instance.GetFirstAction<Trigger2dEvent>("Out Of Range");
             if (triggerAction == null) {
                 Logger.Warn("Could not patch inspect FSM, because trigger action does not exist");
                 return;
